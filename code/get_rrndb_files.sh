3465486cb9dece4ce62528b5fc38fe814ea2bd2b
Jem Stach <jem.stach@newcastle.ac.uk>
2021-10-12 13:14:56 +0100

    Automate pipeline with Makefile, closes #10

--- code/get_rrndb_files.sh
+++ code/get_rrndb_files.sh
@@ -3,9 +3,12 @@
 # auhtor: Jem Stach
 # inputs: name of the file extracted from the archive (without the path)
 # output: The appropriate rrnDNB file into data/raw/
+# touch command used to update the time stamp on downloaded files
 
 archive=$1
 
 
 wget -P data/raw/ -nc https://rrndb.umms.med.umich.edu/static/download/"$archive".zip
 unzip -n -d data/raw/ data/raw/"$archive".zip
+
+touch data/raw/"$archive"
