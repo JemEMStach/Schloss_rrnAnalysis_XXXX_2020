22f2d8bdbdf94fa76e7523ae92a1462f0b0da145
Jem Stach <jem.stach@newcastle.ac.uk>
2021-10-21 15:46:36 +0100

    Create rule to install mothur and use as pre'req, closes #16

--- code/install_mothur.sh
+++ code/install_mothur.sh
@@ -8,3 +8,11 @@
 
 wget -P code/mothur/ -nc https://github.com/mothur/mothur/releases/download/v1.46.1/Mothur.OSX-10.14.zip
 unzip -n -d code/ code/mothur/Mothur.OSX-10.14.zip
+
+if [[ $? -eq 0 ]]
+then
+  touch code/mothur/mothur
+else
+  echo "Could not install mothur"
+  exit 1
+fi
